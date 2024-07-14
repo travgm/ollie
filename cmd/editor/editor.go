257d6dab4bef5a1ec076195346199cb9bb857346
Travis <travis@travgm.org>
2024-07-13 18:47:39 -0600

    updating copyright on source files

--- cmd/editor/editor.go
+++ cmd/editor/editor.go
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2024 Travis Montoya
+// Copyright (c) 2024 Travis Montoya and the ollie contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
